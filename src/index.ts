2387c2c81da22528ad7f24e02a9493c4b6172e76
SaiSrinivas321 <gssrinivas32194@gmail.com>
2021-12-31 19:36:20 +0530

    added job routes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,44 @@ import express from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
 import client from "./db/postgres";
+import cors from "cors"
 
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// To get all jobs
+// /jobs?location=&title=
+app.get("/jobs",async (req,res)=>{
+  try {
+    const {location , title} = req.query;
+    const jobs = await client.query("",[location,title]);
+    if(jobs.rows.length !== 0){
+      res.json(jobs.rows);
+    }else{
+      // fetch the data from flask api
+     
+    }
+   
+  } catch (error) {
+    console.log(error.message)
+    res.end()
+  }
+})
+
+// To get a job
+app.get("/job/:id",async (req,res)=>{
+  try {
+    const {id} = req.params;
+    const job = await client.query("",[id])
+    res.json(job.rows[0]);
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
 
 client.connect().then(() => {
   console.log("Connected to database");
