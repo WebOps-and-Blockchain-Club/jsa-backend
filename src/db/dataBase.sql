f1987dc2e4e08789b314148dcbf241a62aa5db8c
SaiSrinivas321 <gssrinivas32194@gmail.com>
2022-01-25 18:55:25 +0530

    index table for job inputs

--- src/db/dataBase.sql
+++ src/db/dataBase.sql
@@ -1,6 +1,7 @@
 -- uuid extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE EXTENSION pgcrypto;
 -- Create the database using this command
 -- CREATE DATABASE jsa;
 
@@ -34,6 +35,27 @@ CREATE TABLE IF NOT EXISTS input_details(
     details_id VARCHAR(50) REFERENCES job_details(job_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
+-- User details Table
+CREATE TABLE IF NOT EXISTS USER(
+    id UUID NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL,
+    age INT NOT NULL,
+    email TEXT NOT NULL,
+
+    gender TEXT NOT NULL,
+    experience TEXT NOT NULL,
+    UNIQUE(email) 
+
+)
+
+
+CREATE TABLE IF NOT EXISTS input_bot(
+    input_uid UUID NOT NULL PRIMARY KEY,
+    job_title VARCHAR(50) NOT NULL,
+    job_location VARCHAR(50) NOT NULL
+)
+
+CREATE INDEX CONCURRENTLY input_bot_tl_idx ON input_bot(job_title , job_location);
 -- TESTING
 -- INSERT INTO jobinputs(input_uid,job_title , job_location) VALUES(uuid_generate_v4(),'Developer','Chennai');
 -- INSERT INTO job_details(job_id, job_desk,job_employer , job_salary , job_description , job_description_html) VALUES('test','Times Jobs','test employer','22k','test des','test des html');
